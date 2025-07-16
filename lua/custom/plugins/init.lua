58a0ac109d91f281ca099fa5296c45c4a8c29dff
Trent McGinnis <trent.mcginnis@proton.me>
2025-07-16 17:10:33 -0400

    Start to modular

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,17 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  { import = 'custom.plugins.qol' },
+  { import = 'custom.plugins.telescope' },
+  { import = 'custom.plugins.lsp' },
+  { import = 'custom.plugins.theme' },
+  { import = 'custom.plugins.navigation' },
+  --
+  --  Here are some example plugins that I've included in the Kickstart repository.
+  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  --
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+}
